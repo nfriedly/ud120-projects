667479e6b5ef68fc5da0156fd754b29f1720ff4c
Nathan Friedly <nathan@nfriedly.com>
2016-09-19 12:39:15 -0400

    support vector machine

--- svm/svm_author_id.py
+++ svm/svm_author_id.py
@@ -25,6 +25,36 @@ features_train, features_test, labels_train, labels_test = preprocess()
 #########################################################
 ### your code goes here ###
 
+from sklearn.svm import SVC
+from sklearn.metrics import accuracy_score
+
+# delete 99% of the training data
+# features_train = features_train[:len(features_train) / 100]
+# labels_train = labels_train[:len(labels_train) / 100]
+
+#for c in [10, 100, 1000, 10000]:
+
+c = 10000
+print "C =", c
+
+clf = SVC(kernel="rbf", C=c)
+
+t0 = time()
+clf.fit(features_train, labels_train)
+print "training time:", round(time()-t0, 3), "s"
+
+t0 = time()
+pred = clf.predict(features_test)
+print "prediction time:", round(time()-t0, 3), "s"
+
+score = accuracy_score(labels_test, pred)
+print "accuracy:", score
+
+# for i in [10, 26, 50]:
+#     print i, pred[i]
+
+print "# of 1's (chris):", sum(pred)
+
 #########################################################
 
 
