abadb24ed0666a519f6b2b8fc92036e39052a299
Nathan Friedly <nathan@nfriedly.com>
2016-09-24 10:32:38 -0400

    regression

--- regression/finance_regression.py
+++ regression/finance_regression.py
@@ -21,7 +21,7 @@ dictionary = pickle.load( open("../final_project/final_project_dataset_modified.
 
 ### list the features you want to look at--first item in the 
 ### list will be the "target" feature
-features_list = ["bonus", "salary"]
+features_list = ["bonus", "salary"] #["bonus", "salary"] or ["bonus", "long_term_incentive"]
 data = featureFormat( dictionary, features_list, remove_any_zeroes=True)
 target, features = targetFeatureSplit( data )
 
@@ -29,7 +29,7 @@ target, features = targetFeatureSplit( data )
 from sklearn.cross_validation import train_test_split
 feature_train, feature_test, target_train, target_test = train_test_split(features, target, test_size=0.5, random_state=42)
 train_color = "b"
-test_color = "b"
+test_color = "r"
 
 
 
@@ -38,9 +38,15 @@ test_color = "b"
 ### plots it correctly. Don't forget to change the test_color above from "b" to
 ### "r" to differentiate training points from test points.
 
+from sklearn.linear_model import LinearRegression
 
+reg = LinearRegression()
+reg.fit(feature_train, target_train)
+print("slope of training data line", reg.coef_)
+print("y-intercept of training data line", reg.intercept_)
 
-
+print("score on training data", reg.score(feature_train, target_train))
+print("score on test data", reg.score(feature_test, target_test))
 
 
 
@@ -64,6 +70,13 @@ try:
     plt.plot( feature_test, reg.predict(feature_test) )
 except NameError:
     pass
+
+# second regression line for test data
+reg.fit(feature_test, target_test)
+plt.plot(feature_train, reg.predict(feature_train), color="b")
+print("slope of test data line", reg.coef_)
+print("y-intercept or test data line", reg.intercept_)
+
 plt.xlabel(features_list[1])
 plt.ylabel(features_list[0])
 plt.legend()
